467a755e5b70b6e6b517bdb11b3d4227f494d641
YanNaing13 <121233289+YanNaing13@users.noreply.github.com>
2022-12-22 19:18:00 +0630

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm telegram_gcloner/config.ini
-curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
+curl https://gist.githubusercontent.com/YanNaing13/f148e9a2baa69a3efc5db760ce25036c/raw/2b28d21bd9cc17bdf5a6fc0c418cab353745fc1f/config.ini >> telegram_gcloner/config.ini
 npm install http-server -g
 http-server -p 8080 &
 python3 telegram_gcloner/telegram_gcloner.py
